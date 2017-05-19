a600a7d7adfba83b5ddf955a3c1ac6945bb1d137
Ferdz <frederic.deschenes1@gmail.com>
2017-05-19 15:50:52 -0400

    Simple flower animation

--- core/src/me/ferdz/evergreenacres/rendering/ObjectTiledMapRenderer.java
+++ core/src/me/ferdz/evergreenacres/rendering/ObjectTiledMapRenderer.java
@@ -1,14 +1,21 @@
 package me.ferdz.evergreenacres.rendering;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapImageLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.AnimatedTiledMapTile;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.IntArray;
 
 public class ObjectTiledMapRenderer extends OrthogonalTiledMapRenderer {
 
@@ -26,6 +33,31 @@ public class ObjectTiledMapRenderer extends OrthogonalTiledMapRenderer {
 			} else {
 				underLayers.add(m);
 			}
+			
+			// Below code to randomize the lenght of the animations
+//			if (m instanceof TiledMapTileLayer) {
+//				TiledMapTileLayer layer = (TiledMapTileLayer) m;
+//				for (int i = 0; i < layer.getWidth(); i++) {
+//					for (int j = 0; j < layer.getHeight(); j++) {
+//						Cell cell = layer.getCell(i, j);
+//						if (cell != null) {
+//							if (cell.getTile() instanceof AnimatedTiledMapTile) {
+//								AnimatedTiledMapTile tile = (AnimatedTiledMapTile) cell.getTile();
+////								System.out.println(tile.getAnimationIntervals()[0]);
+//								IntArray animationIntervals = new IntArray();
+//								for (int k = 0; k < tile.getAnimationIntervals().length; k++) {
+//									animationIntervals.add((int) (ThreadLocalRandom.current().nextFloat() * (1200 - 800) + 800));
+//								}
+//								AnimatedTiledMapTile newTile = new AnimatedTiledMapTile(
+//										animationIntervals, 
+//										new Array(tile.getFrameTiles()));
+////								newTile.getAnimationIntervals()[0] = (int) (ThreadLocalRandom.current().nextFloat() * 1000);
+//								cell.setTile(newTile);
+//							}
+//						}
+//					}
+//				}
+//			}
 		}
 	}
 	
