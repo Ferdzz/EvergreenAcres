67b01d4d8fa06d508502601c301454e3f1b3fe7d
Ferdz <frederic.deschenes1@gmail.com>
2017-06-19 22:21:45 -0400

    Build

--- core/src/me/ferdz/evergreenacres/environment/EnumCrop.java
+++ core/src/me/ferdz/evergreenacres/environment/EnumCrop.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import me.ferdz.evergreenacres.rendering.Textures.CropTexture;
 
 public enum EnumCrop {
-	POTATO(1, CropTexture.POTATO_6, CropTexture.POTATO_1, CropTexture.POTATO_2, CropTexture.POTATO_3, CropTexture.POTATO_4, CropTexture.POTATO_5, CropTexture.POTATO_6);
+	POTATO(1, CropTexture.POTATO_0, CropTexture.POTATO_1, CropTexture.POTATO_2, CropTexture.POTATO_3, CropTexture.POTATO_4, CropTexture.POTATO_5, CropTexture.POTATO_6);
 	
 	@Getter final private float growthMultiplier;
 	@Getter final private CropTexture[] textures;
